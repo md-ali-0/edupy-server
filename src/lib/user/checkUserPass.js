fdaf341c8830afda2526b9c007289c3c516f5b8b
md-ali-0 <mohammad.98482@gmail.com>
2024-04-01 14:34:54 +0600

    removed

--- src/lib/user/checkUserPass.js
+++ src/lib/user/checkUserPass.js
@@ -1,59 +1,44 @@
-import cookie from "cookie";
 import "dotenv/config";
 import jwt from "jsonwebtoken";
 import User from "../../models/Users.js";
 import { bcHashCompare } from "../../utils/bcrypt.js";
 
-const checkUserPass = async (user) => {
-    const email = user.email;
-    const planePassword = user.password;
-    try {
-        const query = {
-            email: email,
-        };
+const checkUserPass = async(user)=>{
 
-        const reqUser = await User.findOne(query);
+    const email = user.email;
+    const planePassword = user.password
+    try{
+        const query = {
+            email:email
+        }
+        
+        const reqUser = await User.findOne(query)
 
         if (!reqUser) {
-            const error = new Error("User not found");
-            error.status = 404;
-            throw error;
+            const error = new Error('User not found')
+            error.status = 404
+            throw error
         }
 
-        const cursor = await bcHashCompare(planePassword, reqUser.password);
+        const cursor = await bcHashCompare(planePassword, reqUser.password)
         if (cursor) {
-            const token = jwt.sign(
-                { email: reqUser.email },
-                process.env.ACCESS_TOKEN,
-                {
-                    expiresIn: "1h",
-                }
-            );
-            
-            res.setHeader(
-                "Set-Cookie",
-                cookie.serialize("token", token, {
-                    httpOnly: true,
-                    maxAge: 3600,
-                    sameSite: "strict",
-                    secure: process.env.NODE_ENV === "production",
-                    path: "/",
-                })
-            );
-
+            const token = jwt.sign({email:reqUser.email}, process.env.ACCESS_TOKEN, {
+                expiresIn: "1h",
+            });
             const result = {
                 user: reqUser,
-                token,
-            };
-            return result;
-        } else {
-            const error = new Error("Password is Wrong");
-            error.status = 401;
-            throw error;
+                token
+            }
+            return result
+        } else{
+            const error = new Error('Password is Wrong')
+            error.status = 401
+            throw error
         }
-    } catch (err) {
-        throw new Error(err);
-    }
-};
 
-export default checkUserPass;
+    } catch(err){
+        throw new Error(err)
+    }
+}
+
+export default checkUserPass
